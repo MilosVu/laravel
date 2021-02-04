51d8fffd7126af6d02062119810563a901fb71b9
Miloš Vujić <MilosVu@users.noreply.github.com>
2021-02-04 17:46:22 +0100

    Dodata sva podesavanja

---
+++ app/Models/Car.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Model\User;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
---
+++ database/migrations/2021_02_03_165722_create_car_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('brend');
+            $table->integer('year');
+            $table->integer('user_id')->unsigned()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car');
+    }
+}
